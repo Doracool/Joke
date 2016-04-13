54d0d3ede2c324c51201fb9cacc318f5867398d5
Doracool <asas13525482040@163.com>
2016-04-13 15:44:06 +0800

    添加注册信息

--- Demo/百思不得姐/百思不得姐/Classes/FriendTrends-关注/Controller/TTLoginRegisterViewController.m
+++ Demo/百思不得姐/百思不得姐/Classes/FriendTrends-关注/Controller/TTLoginRegisterViewController.m
@@ -9,21 +9,43 @@
 #import "TTLoginRegisterViewController.h"
 
 @interface TTLoginRegisterViewController ()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginViewLeftMargin;
 @property (weak, nonatomic) IBOutlet UITextField *phoneFiled;
 @property (weak, nonatomic) IBOutlet UIImageView *bgView;
 @end
 
 @implementation TTLoginRegisterViewController
+- (IBAction)back:(UIButton *)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (IBAction)showLoginOrRegister:(UIButton *)sender {
+    //退出键盘
+    [self.view endEditing:YES];
+    
+    if (self.loginViewLeftMargin.constant == 0) {
+        //线束注册界面
+        self.loginViewLeftMargin.constant = -self.view.width;
+        sender.selected = YES;
+    }else{
+        //显示登录界面
+        self.loginViewLeftMargin.constant = 0;
+        sender.selected = NO;
+    }
+    
+    [UIView animateWithDuration:0.25 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     //文字属性
-    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
-    attrs[NSForegroundColorAttributeName] = [UIColor yellowColor];
-    // NSAttributedString : 带有属性的文字
-    NSAttributedString *placeholder = [[NSAttributedString alloc] initWithString:@"手机号" attributes:attrs];
-    self.phoneFiled.attributedPlaceholder = placeholder;
-    
+//    NSMutableDictionary *attrs = [NSMutableDictionary dictionary];
+//    attrs[NSForegroundColorAttributeName] = [UIColor yellowColor];
+//    // NSAttributedString : 带有属性的文字
+//    NSAttributedString *placeholder = [[NSAttributedString alloc] initWithString:@"手机号" attributes:attrs];
+//    self.phoneFiled.attributedPlaceholder = placeholder;
+//    
 //    [self.view insertSubview:self.bgView atIndex:0];
 }
 
--- Demo/百思不得姐/百思不得姐/Classes/FriendTrends-关注/View/TTTextFiled.m
+++ Demo/百思不得姐/百思不得姐/Classes/FriendTrends-关注/View/TTTextFiled.m
@@ -7,12 +7,81 @@
 //
 
 #import "TTTextFiled.h"
+#import <objc/runtime.h>
 
+static NSString * const TTPlacerholderColorKeyPath = @"_placeholderLabel.textColor";
 @implementation TTTextFiled
 
-- (void)drawPlaceholderInRect:(CGRect)rect
++ (void)initialize
 {
-    [self.placeholder drawInRect:CGRectMake(0, 10, rect.size.width, 25) withAttributes:@{NSForegroundColorAttributeName : [UIColor grayColor], NSFontAttributeName : self.font}];
+    [self getIvars];
 }
+
++ (void)getProperties
+{
+    unsigned int count = 0;
+    
+    objc_property_t *properties = class_copyPropertyList([UITextField class], &count);
+    
+    for (int i = 0; i < count; i++) {
+        //取出属性
+        objc_property_t property = properties[i];
+        
+        //打印属性名字
+        TTLog(@"%s  <----->  %s",property_getName(property),property_getAttributes(property));
+    }
+    free(properties);
+}
+
++ (void)getIvars
+{
+    unsigned int count = 0;
+    
+    //拷贝出所有的成员变量的名字
+    Ivar *ivars = class_copyIvarList([UITextField class], &count);
+    
+    for (int i = 0; i < count; i++) {
+        //取出成员变量
+        Ivar ivar = ivars[i];
+        
+        //打印成员变量的名字
+        TTLog(@"%s  %s", ivar_getName(ivar),ivar_getTypeEncoding(ivar));
+    }
+    //释放
+    free(ivars);
+}
+
+- (void)awakeFromNib
+{
+    //设置光标颜色和文字颜色一致
+    self.tintColor = self.textColor;
+    
+    //不成为第一响应者
+    [self resignFirstResponder];
+}
+
+/**
+ *  当前文本框聚焦时就会调用
+ */
+- (BOOL)becomeFirstResponder
+{
+    //修改占位文字颜色
+    [self setValue:self.textColor forKeyPath:TTPlacerholderColorKeyPath];
+    return [super becomeFirstResponder];
+}
+
+/**
+ *  当前文本框失去焦点时就会调用
+ */
+- (BOOL)resignFirstResponder
+{
+    //修改占位文字的颜色
+    [self setValue:[UIColor grayColor] forKeyPath:TTPlacerholderColorKeyPath];
+    return [super resignFirstResponder];
+}
+//- (void)drawPlaceholderInRect:(CGRect)rect
+//{
+//    [self.placeholder drawInRect:CGRectMake(0, 10, rect.size.width, 25) withAttributes:@{NSForegroundColorAttributeName : [UIColor grayColor], NSFontAttributeName : self.font}];
+//}
 
 @end

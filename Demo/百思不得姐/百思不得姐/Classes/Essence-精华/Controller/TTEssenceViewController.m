8383247cbf3b20f5abcd73e864386603a63712dc
Doracool <asas13525482040@163.com>
2016-04-13 16:43:09 +0800

    添加首页的标题栏

--- Demo/百思不得姐/百思不得姐/Classes/Essence-精华/Controller/TTEssenceViewController.m
+++ Demo/百思不得姐/百思不得姐/Classes/Essence-精华/Controller/TTEssenceViewController.m
@@ -9,21 +9,75 @@
 #import "TTEssenceViewController.h"
 #import "TTRecommendTagsViewController.h"
 @interface TTEssenceViewController ()
-
+/** 标签栏底部的红色指示器 */
+@property (nonatomic, weak) UIView *indicatorView;
 @end
 
 @implementation TTEssenceViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+
+    //设置导航栏
+    [self setupNav];
+    //设置顶部的标签栏
+    [self setupTitlesView];
+}
+
+- (void)setupTitlesView
+{
+    //标签栏整体
+    UIView *titlesView = [[UIView alloc] init];
+    titlesView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.7];
+    titlesView.width = self.view.width;
+    titlesView.height = 35;
+    titlesView.y = 64;
+    [self.view addSubview:titlesView];
+    
+    //内部的子标签
+    NSArray *titles = @[@"全部",@"视频",@"声音",@"图片",@"段子"];
+    CGFloat width = titlesView.width / titles.count;
+    CGFloat height = titlesView.height;
+    for (NSInteger i = 0; i < titles.count ;i++) {
+        UIButton *button = [[UIButton alloc] init];
+        button.height = height;
+        button.width = width;
+        button.x = i * width;
+        [button setTitle:titles[i] forState:UIControlStateNormal];
+        [button setTitleColor:[UIColor grayColor] forState:UIControlStateNormal];
+        button.titleLabel.font = [UIFont systemFontOfSize:14];
+        [button addTarget:self action:@selector(titleClick:) forControlEvents:UIControlEventTouchUpInside];
+        [titlesView addSubview:button];
+    }
+    //底部的红色指示器
+    UIView *indicatorView = [[UIView alloc] init];
+    indicatorView.backgroundColor = [UIColor redColor];
+    indicatorView.height = 2;
+    indicatorView.y = titlesView.height - indicatorView.height;
+    [titlesView addSubview:indicatorView];
+    self.indicatorView = indicatorView;
+}
+
+- (void)titleClick:(UIButton *)button
+{
+    [UIView animateWithDuration:0.25 animations:^{
+        self.indicatorView.width = button.titleLabel.width;
+        self.indicatorView.centerX = button.centerX;
+    }];
+}
+
+/**
+ *  设置导航栏
+ */
+- (void)setupNav
+{
     //设置导航栏标题
     self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"MainTitle"]];
     
     //设置导航栏左右的按钮
     self.navigationItem.leftBarButtonItem = [UIBarButtonItem itemWithImage:@"MainTagSubIcon" hightImage:@"MainTagSubSubIconClick" Target:self action:@selector(tagClick)];
-    self.view.backgroundColor = [UIColor orangeColor];
+    self.view.backgroundColor = TTGlobalBg;
 }
-
 - (void)tagClick
 {
     TTRecommendTagsViewController *tags = [[TTRecommendTagsViewController alloc] init];
